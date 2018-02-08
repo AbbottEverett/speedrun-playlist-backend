dfdb75a01967ac294c7e60844721dd585b875b1c
S. Everett Abbott <severettabbott@gmail.com>
2018-02-08 11:05:40 -0800

    Created POST route for runs API

--- src/routes/runs.js
+++ src/routes/runs.js
@@ -19,7 +19,22 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const { name, date, category, runTime, system, video_url } = req.body;
+  // Check if req body is valid
+  // Create it and add it to array
+  const run = {
+    id: runs[runs.length-1].id + 1,
+    name,
+    date,
+    category,
+    runTime,
+    system,
+    video_url
+  };
+  runs.push(run);
+  // Send an error
 
+  res.status(201).json({ run });
 });
 
 module.exports = router;
